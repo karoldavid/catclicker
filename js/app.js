b1edfe67777b16ea6c1adcaf992b23ea16c40d34
lorak <k.zysk@zoho.com>
2015-06-09 17:44:53 +0200

    show current cat img, name and clicks

--- js/app.js
+++ js/app.js
@@ -8,11 +8,14 @@ $(function(){
         },
         getAllCats: function() {
             return JSON.parse(localStorage.cats);
-        }
+        },
+        getCat: function(num) {
+            return JSON.parse(localStorage.cats)[num];
+        },
     };
 
     var octopus = {
-        getNames: function() {
+        getCatNames: function() {
             var allNames = [],
                 allCats = model.getAllCats();
             for (kitten in allCats) {
@@ -20,7 +23,9 @@ $(function(){
             }
             return allNames;
         },
-
+        getCurrentCat: function(num) {       
+            return model.getCat(num);
+        },
         init: function() {
             model.init();
             view.init();
@@ -30,14 +35,38 @@ $(function(){
     var view = {
         init: function() {
             this.catList = $('#cat-list');
+            this.catShow = $('#cat-show');
+            this.currentCatNum = 0;
             view.render();
         },
         render: function() {
             var htmlStr = '';
-            octopus.getNames().forEach(function(kittenName){
-                htmlStr += '<li class="note">'+ kittenName + '</li>';
-            });
-           this.catList.html( htmlStr );
+            if (!this.catList.firstChild) {
+                octopus.getCatNames().forEach(function(kittenName){
+                    htmlStr += '<li class="cat">'+ kittenName + '</li>';
+                });
+                this.catList.html( htmlStr );
+                
+                var list = $('.cat');
+                for (var i = 0; i < list.length; i++) {
+                    list[i].addEventListener('click', (function(numCopy) {
+                        return function() {
+                            this.currentCatNum = numCopy;
+                            octopus.addClicks(this.currentCatNum);
+                            //alert(this.currentCatNum);
+                        };
+                    })(i));
+                }
+            }
+            var htmlStr = '',
+                kitten = octopus.getCurrentCat(this.currentCatNum);
+           
+            htmlStr += '<h2>' + kitten.name +
+                       '</h2><img class="current-cat" src='+ kitten.img +'>' +
+                       '<p>Strokes: ' + kitten.clicks + '</p>';
+    
+            this.catShow.html( htmlStr );
+
         } 
     };
     octopus.init();
